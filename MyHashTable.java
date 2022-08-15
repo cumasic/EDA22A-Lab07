29b12306655ac5ba85231eb57708fd329015ebdf
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2022-08-14 20:12:35 -0500

    Se agregarón los métodos de la interfaz en la clase principal

---
+++ MyHashTable.java
@@ -0,0 +1,27 @@
+public class MyHashTable<E,T> implements HashTable<E,T> {
+    private Node<E,T> first;
+	
+	public MyHashTable() {
+		this.first = null;
+	}
+    public int size() {
+    }
+    public boolean isEmpty() {
+    }
+    public boolean containsKey(E key){
+    }
+    public boolean containsValue(T value){
+    }
+    public T get(E key) throws NullPointerException{
+    }
+    public T put(E key, T value){
+    }
+    public T remove(E key) {
+    }
+    public void clear() {
+    }
+    public int hashCode() {
+    }
+    public String toString() {
+    }
+}
